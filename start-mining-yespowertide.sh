ed88f5af396c2c1e725307b57eb1b51e868212cc
TY <34364257+SbaseTY@users.noreply.github.com>
2022-03-25 19:13:38 +0900

    Add files via upload

---
+++ start-mining-yespowertide.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm yespowertide --pool tidepool.world:6243 --wallet TSmfmLMBnufPRQVPgPXEB2qpqrFMjrVRja --password c=TDC
